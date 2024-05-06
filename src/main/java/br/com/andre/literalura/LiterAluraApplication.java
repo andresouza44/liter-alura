63de8599352ac5eaad646789407a0d3b72a6954b
Andre M. de Souza <andresouza44@gmail.com>
2024-05-06 11:32:44 -0300

    first commit

---
+++ src/main/java/br/com/andre/literalura/LiterAluraApplication.java
@@ -0,0 +1,13 @@
+package br.com.andre.literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/andre/literalura/LiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andre.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
